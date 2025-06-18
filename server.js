0fad88e2cb928d7a39c82cd997fe80551a31eab5
lizuli123 <2236001282@utna.edu.mx>
2025-06-18 16:51:43 -0600

    primer comit del apirest 9b

---
+++ server.js
@@ -0,0 +1,7 @@
+const app= require('./app/app')
+const config = require('./app/config/configuracion')
+
+app.listen(config.PORT,()=>{
+    console.log('aplicacion corriendo en puerto', config.PORT);
+     config.PORT
+})
